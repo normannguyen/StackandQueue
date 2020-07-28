eaf18867491b4ddc2cc4e84edc3956e6cd5d5f14
Norman Nguyen <normannguyen3@gmail.com>
2020-07-28 13:36:22 -0700

    Added stuff.

--- StackandQueue/Source.cpp
+++ StackandQueue/Source.cpp
@@ -1,62 +1,52 @@
 #include <iostream>
 #include <stack>
-#include <queue>
 using namespace std;
 
 int main() {
 	// Initiate an Integer Stack using vector
-	vector<int> vStack;
-	vector<int> vQueue;
+	stack<int> Stack;
 
-	int inputInt;
+	int value;
+
 	int choice = 99;
 	do {
-		cout << "\nMenu to manipulate integer stack and queue:\n";
-		cout << "1: To push an integer on stack\n";
-		cout << "2: To pop an integer from stack\n";
-		cout << "3: To display stack info\n";
-		cout << "11: To shift an integer on queue\n";
-		cout << "12: To unshift an integer from queue\n";
-		cout << "13: To display queue info\n";
-		cout << "99: Finish entering\n";
-		cout << "Please enter your choice (#): ";
+		cout << "\n**************\n";
+		cout << "\n1. Empty";
+		cout << "\n2. Add";
+		cout << "\n3. Remove";
+		cout << "\n4. Display";
+		cout << "\n5. Update";
+		cout << "\n6. Exit";
+		cout << "\n\nPlease enter the # listed above: ";
 		cin >> choice;
 
 		switch (choice) {
 		case 1:
-			cout << "\nEnter an integer to STACK: ";
-			cin >> inputInt;
-			vStack.push_back(inputInt);
+			Stack.empty();
+			cout << "Everything is empty." << endl;
 			break;
 		case 2:
-			if (vStack.size() != 0)  vStack.pop_back();
-			// break;  // intentionally skip to display STACK
+			cout << "Insert a number: ";
+			cin >> value;
+			Stack.push(value);
+			break;  
 		case 3:
-			cout << "\n# of items on STACK: " << vStack.size();
-			cout << "\nItems on STACK (from top to bottom): " << endl;
-			for (unsigned int i = 0; i < vStack.size(); i++) {
-				cout << vStack[i] << ' ';
-			}
-			cout << endl;
+			Stack.pop();
+			cout << "Number is deleted.";
 			break;
-		case 11:
-			cout << "\nEnter an integer to QUEUE: ";
-			cin >> inputInt;
-			vQueue.push_back(inputInt);
-			break;
-		case 12:
-			if (vQueue.size() != 0)  vQueue.erase(vQueue.begin());
-			// break;  // intentionally skip to display QUEUE
-		case 13:
-			cout << "\n# of items on QUEUE: " << vQueue.size();
-			cout << "\nItems on QUEUE (from begin to end): " << endl;
-			for (unsigned int i = 0; i < vQueue.size(); i++) {
-				cout << vQueue[i] << ' ';
+		case 4:
+			cout << "Size: " << Stack.size() << endl;
+			while (!Stack.empty())
+			{
+				cout << '\t' << Stack.size();
+				Stack.pop();
 			}
-			cout << endl;
+			break;
+		case 5:
+			cout << Stack.top();
 			break;
 		}
-	} while (choice != 99);
+	} while (choice != 6);
 
 	cout << "Ending Program\n";
 	system("pause");
---
+++ StackandQueue/StackAndQueue.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+template <typename T>
+class Stack {
+	stack<T> Stack;
+};
+template <typename T>
+class Queue {
+	queue<T> Queue;
+};
+
+void CheckEmpty() {
+
+}
+void Add() 
+{
+
+}
+void Print() 
+{
+}
+void Remove()
+{
+}
+void Update() 
+{
+}
